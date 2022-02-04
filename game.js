823a1e0565effd41d7bdad5f94be6427821e8f5c
NPitroff <nicolai.pitroff@gmail.com>
2022-02-03 20:53:10 -0800

    Added a function that will generate a random number, use it to pick from the color pattern, and then push the choice into a new array

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
+// var to hold the color options
+var buttonColors = ["red", "blue", "green", "yellow"];
+// array to hold the game pattern
+var gamePattern = [];
 
+function nextSequence(){
+  // var to generate a random number
+  var randomNumber = Math.floor(Math.random()*4);
+  console.log(randomNumber);
+  // var to pick a random element from the array
+  var randomChosenColor = buttonColors[randomNumber];
+  // console.log("Random Chosen Color is "+randomChosenColor);
+
+  // var to push into the gamePattern array
+  gamePattern.push(randomChosenColor);
+  console.log("The Game Pattern Is "+gamePattern);
+}
+
+// var randomChosenColor = buttonColors[randomNumber];
