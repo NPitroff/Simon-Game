2807be510ece47cad1ae76e548bc2482a7863032
NPitroff <nicolai.pitroff@gmail.com>
2022-02-04 06:54:00 -0800

    Added a jquery selector to pick the randomChosenColor

--- game.js
+++ game.js
@@ -8,12 +8,12 @@ function nextSequence(){
   var randomNumber = Math.floor(Math.random()*4);
   console.log(randomNumber);
   // var to pick a random element from the array
-  var randomChosenColor = buttonColors[randomNumber];
-  // console.log("Random Chosen Color is "+randomChosenColor);
+  var randomChosenColor = buttonColors[randomNumber]
+  console.log("Random Chosen Color is "+randomChosenColor);
 
   // var to push into the gamePattern array
   gamePattern.push(randomChosenColor);
   console.log("The Game Pattern Is "+gamePattern);
+  // select the button with the same id as the random Color
+  $("#"+randomChosenColor).fadeOut(100);
 }
-
-// var randomChosenColor = buttonColors[randomNumber];
