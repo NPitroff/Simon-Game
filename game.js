94c69dd1e863485a24366f88f6d287ce3d35b041
NPitroff <nicolai.pitroff@gmail.com>
2022-02-07 16:58:46 -0800

    Added if statement to check if the answer was right

--- game.js
+++ game.js
@@ -4,12 +4,50 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 // array for the user pattern
 var userClickedPattern = [];
-// variable to store the level
-var level = 0;
 // var to detect if the keypress has already been hit
 var started = false;
+// variable to store the level
+var level = 0;
 
+// detect when a key has been pressed
+$(document).keypress(function(){
+  // if statement to detect start of game
+  if (!started) {
+    // change the h1 to show the level
+    $("#level-title").html("Level "+level);
+    // call nextSequence() to start the game
+    nextSequence();
+    started = true;
+  }
+});
+
+// detect when a button is clicked
+$(".btn").click(function(e){
+  // console.log(e);
+  var userChosenColor = $(this).attr('id');
+  // console.log("The userChosenColor is "+userChosenColor);
+  userClickedPattern.push(userChosenColor);
+  // console.log(userClickedPattern);
+  // call to play sound
+  playSound(userChosenColor);
+  // call function to animate press
+  animatePress(userChosenColor);
+  // call to check the answers
+  checkAnswer(userClickedPattern.length-1);
+})
+
+// function to check the user answer
+function checkAnswer(currentLevel){
+  //check if user array matches against game array
+  if(gamePattern[currentLevel] === userClickedPattern[currentLevel])
+  console.log("success");
+  
+}
+
+// add to the sequence
 function nextSequence(){
+  // increase the count of level for every nextSequence()
+  level++
   // var to generate a random number
   var randomNumber = Math.floor(Math.random()*4);
   //console.log("The Random Number is "+randomNumber);
@@ -23,24 +61,9 @@ function nextSequence(){
   $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
     // call playSound for randomChosenColor
   playSound(randomChosenColor);
-  // increase the count of level for every nextSequence()
-  level++
 }
 
-// detect when a button is clicked
-$(".btn").click(function(e){
-  console.log(e);
-  var userChosenColor = $(this).attr('id');
-  console.log("The userChosenColor is "+userChosenColor);
-  userClickedPattern.push(userChosenColor);
-  console.log(userClickedPattern);
-  // // show button click
-  // $("#"+userChosenColor).fadeOut(100).fadeIn(100);
-  // call playSound() for the userChosenColor
-  playSound(userChosenColor);
-  // call function to animate press
-  animatePress(userChosenColor);
-})
+
 
 // add sounds to button clicks and playbacks
 function playSound(name){
@@ -58,32 +81,4 @@ function animatePress(currentColor){
   setTimeout(function(){
     $("."+currentColor).removeClass("pressed");
   }, 100);
-}
-
-// detect when a key has been pressed
-$(document).keypress(function(){
-  // if statement to detect start of game
-  if (!started) {
-    started = true;
-  // change the h1 to show the level
-  $("#level-title").html("Level "+level);
-  // call nextSequence() to start the game
-  nextSequence();
-  }
-});
-
-// function to check the user answer
-function checkAnswer(currentLevel){
-  // pass the indexOf the userClickedPattern
-  if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
-    // count how many colors the user got correct
-    var correct = 0;
-    // loop through arrays to compare the same values
-    for (var=i; i < gamePattern.length; i++){
-      if(gamePattern[i] === userClickedPattern[i]){
-        correct++;
-      }
-    }
-  }
-
 }
